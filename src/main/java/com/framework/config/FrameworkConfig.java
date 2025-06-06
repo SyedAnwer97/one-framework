6f5d7de940e9784cdf21f94de7d4dfd7e2eb50da
anwer <anwersahira786@gmail.com>
2025-06-06 13:02:46 +0530

    update the FrameworkConfig with new properties

--- src/main/java/com/framework/config/FrameworkConfig.java
+++ src/main/java/com/framework/config/FrameworkConfig.java
@@ -1,7 +1,11 @@
 package com.framework.config;
 
 import com.framework.converters.StringToBrowserConverter;
+import com.framework.converters.StringToBrowserRemoteModeTypeConverter;
+import com.framework.converters.StringToRunModeBrowserTypeConverter;
+import com.framework.enums.BrowserRemoteModeType;
 import com.framework.enums.BrowserType;
+import com.framework.enums.RunModeBrowserType;
 import org.aeonbits.owner.Config;
 
 @Config.LoadPolicy(Config.LoadType.MERGE)
@@ -16,4 +20,12 @@ public interface FrameworkConfig extends Config {
     @DefaultValue("CHROME")
     @ConverterClass(StringToBrowserConverter.class)
     BrowserType browser();
+
+    @Key("runModeBrowser")
+    @ConverterClass(StringToRunModeBrowserTypeConverter.class)
+    RunModeBrowserType browserRunMode();
+
+    @Key("browserRemoteMode")
+    @ConverterClass(StringToBrowserRemoteModeTypeConverter.class)
+    BrowserRemoteModeType browserRemoteMode();
 }
