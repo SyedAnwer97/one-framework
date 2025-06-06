a31b4eb587111088d48c46b92da69930733a54a2
anwer <anwersahira786@gmail.com>
2025-06-06 13:04:18 +0530

    code refracted

--- src/test/java/com/framework/DemoTest.java
+++ src/test/java/com/framework/DemoTest.java
@@ -1,21 +1,13 @@
 package com.framework;
 
-import com.framework.config.ConfigFactory;
 import lombok.SneakyThrows;
 import org.testng.annotations.Test;
 
-public class DemoTest {
+public class DemoTest extends WebBase{
 
     @SneakyThrows
     @Test
     public void loginTest() {
-        System.out.println(ConfigFactory.getConfig().browser());
-
-        /*WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.co.in/");
-        driver.manage().window().maximize();
         Thread.sleep(3000);
-        driver.quit();*/
     }
 }
