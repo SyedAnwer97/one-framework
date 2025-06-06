efe816aa7d14b6a30f5363094b3de6fb4256c646
anwer <anwersahira786@gmail.com>
2025-06-06 12:58:32 +0530

    Converter class for string to BrowserRemoteModeType & RunModeBrowserType

---
+++ src/main/java/com/framework/converters/StringToBrowserRemoteModeTypeConverter.java
@@ -0,0 +1,13 @@
+package com.framework.converters;
+
+import com.framework.enums.BrowserRemoteModeType;
+import org.aeonbits.owner.Converter;
+
+import java.lang.reflect.Method;
+
+public class StringToBrowserRemoteModeTypeConverter implements Converter<BrowserRemoteModeType> {
+    @Override
+    public BrowserRemoteModeType convert(Method method, String remoteMode) {
+        return BrowserRemoteModeType.valueOf(remoteMode.toUpperCase());
+    }
+}
