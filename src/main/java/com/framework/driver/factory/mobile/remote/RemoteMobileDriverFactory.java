55c204aa772510fb0f4b0ef1ff37bf7f29c41fa3
anwer <anwersahira786@gmail.com>
2025-06-08 22:20:21 +0530

    Added driver factory for Local and Remote for appium

---
+++ src/main/java/com/framework/driver/factory/mobile/remote/RemoteMobileDriverFactory.java
@@ -0,0 +1,26 @@
+package com.framework.driver.factory.mobile.remote;
+
+import com.framework.enums.MobilePlatformType;
+import com.framework.enums.MobileRemoteModeType;
+import org.openqa.selenium.WebDriver;
+
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.function.Function;
+
+public class RemoteMobileDriverFactory {
+
+    private RemoteMobileDriverFactory() {
+    }
+
+    private static final Map<MobileRemoteModeType, Function<MobilePlatformType, WebDriver>> MAP = new EnumMap<>(MobileRemoteModeType.class);
+
+    static {
+        MAP.put(MobileRemoteModeType.BROWSER_STACK, BrowserStackMobileFactory::getDriver);
+        // Need To impl sauce lab
+    }
+
+    public static WebDriver getDriver(MobileRemoteModeType mobileRemoteModeType, MobilePlatformType mobilePlatformType) {
+        return MAP.get(mobileRemoteModeType).apply(mobilePlatformType);
+    }
+}
