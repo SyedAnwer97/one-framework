e4b989a9e5475f7de3ab5b0401780dba5d213c4f
anwer <anwersahira786@gmail.com>
2025-06-12 21:52:20 +0530

    updated with getter methods

--- src/main/java/com/framework/driver/entity/MobileDriverData.java
+++ src/main/java/com/framework/driver/entity/MobileDriverData.java
@@ -3,8 +3,10 @@ package com.framework.driver.entity;
 import com.framework.enums.MobilePlatformType;
 import com.framework.enums.MobileRemoteModeType;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class MobileDriverData {
     private MobilePlatformType mobilePlatformType;
     private MobileRemoteModeType mobileRemoteModeType;
--- src/main/java/com/framework/driver/entity/WebDriverData.java
+++ src/main/java/com/framework/driver/entity/WebDriverData.java
@@ -3,8 +3,10 @@ package com.framework.driver.entity;
 import com.framework.enums.BrowserRemoteModeType;
 import com.framework.enums.BrowserType;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class WebDriverData {
 
     private BrowserType browserType;
