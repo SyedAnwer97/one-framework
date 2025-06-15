09e3537e261426158e9cc1104ced6593d1c30b5e
anwer <anwersahira786@gmail.com>
2025-06-15 22:52:34 +0530

    Added Top Header Component for home page

--- src/main/java/com/framework/pages/HomePage.java
+++ src/main/java/com/framework/pages/HomePage.java
@@ -3,12 +3,16 @@ package com.framework.pages;
 import com.framework.pages.enums.homepage.Menu;
 import com.framework.pages.enums.homepage.SubMenu;
 import com.framework.pages.pagecomponent.homepage.MenuPage;
+import com.framework.pages.pagecomponent.homepage.TopMenuComponent;
+import com.framework.pages.validator.HomePageValidator;
 
 public class HomePage extends SeleniumPageActions {
 
     private MenuPage menuPage;
+    private TopMenuComponent topMenuComponent;
 
     public HomePage() {
+        this.topMenuComponent = new TopMenuComponent();
         this.menuPage = new MenuPage();
     }
 
@@ -22,5 +26,13 @@ public class HomePage extends SeleniumPageActions {
                 .clickSubMenu(SubMenu.USER_MANAGEMENT)
                 .clickSubMenu(SubMenu.USER);
         return new SystemUsersPage();
+    }
+
+    public HomePageValidator getHomePageValidator() {
+        return HomePageValidator.builder()
+                .isUpgradeDisplayed(topMenuComponent.isUpgradeButtonPresent())
+                .logoSourceString(topMenuComponent.getLogoSourceString())
+                .headerName(topMenuComponent.getHeaderName())
+                .build();
     }
 }
